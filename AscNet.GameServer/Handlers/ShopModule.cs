9b1a2179fe5c41a43dd19156dd3f7c9fa0556a8d
Kyle Belanger <kyle873@gmail.com>
2023-10-14 12:27:53 -0400

    Partial shop fixes

---
+++ AscNet.GameServer/Handlers/ShopModule.cs
@@ -0,0 +1,29 @@
+﻿using AscNet.Common.MsgPack;
+using MessagePack;
+
+namespace AscNet.GameServer.Handlers
+{
+    internal class ShopModule
+    {
+        /* TODO: Needs types
+        [RequestPacketHandler("GetShopBaseInfoRequest")]
+        public static void GetShopBaseInfoRequestHandler(Session session, Packet.Request packet)
+        {
+        }
+        */
+
+        [RequestPacketHandler("GetShopInfoReceiveRequest")]
+        public static void GetShopInfoReceiveRequestHandler(Session session, Packet.Request packet)
+        {
+            GetShopInfoRequest req = MessagePackSerializer.Deserialize<GetShopInfoRequest>(packet.Content);
+
+            GetShopInfoResponse rsp = new GetShopInfoResponse()
+            {
+                Code = 0,
+                ClientShop = { }
+            };
+
+            session.SendResponse(rsp);
+        }
+    }
+}
