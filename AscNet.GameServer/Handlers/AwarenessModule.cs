27d960c4ba6bcb8e2e30deb572c97a493759e6f3
rfi <rfi@cunny.my.id>
2024-01-23 07:22:05 +0700

    2.0.0 res

---
+++ AscNet.GameServer/Handlers/AwarenessModule.cs
@@ -0,0 +1,31 @@
+﻿using MessagePack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AscNet.GameServer.Handlers
+{
+    #region MsgPackScheme
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    [MessagePackObject(true)]
+    public class AwarenessGetDataResponse
+    {
+        public dynamic AwarenessInfo { get; set; }
+    }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    #endregion
+
+    internal class AwarenessModule
+    {
+        [RequestPacketHandler("AwarenessGetDataRequest")]
+        public static void AwarenessGetDataRequestHandler(Session session, Packet.Request packet)
+        {
+            session.SendResponse(new AwarenessGetDataResponse()
+            {
+                AwarenessInfo = {}
+            }, packet.Id);
+        }
+    }
+}
