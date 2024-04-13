3e2caf704dab71f9a90662797f55ae1116f6d03b
Kyle Belanger <kyle873@gmail.com>
2024-04-13 00:36:14 -0400

    PayInitiatedRequest stub to prevent softlock on purchasing Rainbow Cards

--- AscNet.GameServer/Handlers/PayModule.cs
+++ AscNet.GameServer/Handlers/PayModule.cs
@@ -1,14 +1,37 @@
 ﻿using AscNet.Common.MsgPack;
+using MessagePack;
 
 namespace AscNet.GameServer.Handlers
 {
+    #region MsgPackScheme
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    [MessagePackObject(true)]
+    public class PayInitiatedRequest
+    {
+        public string Key;
+        public string? TargetParam;
+    }
+
+    [MessagePackObject(true)]
+    public class PayInitiatedResponse
+    {
+        public int Code;
+    }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    #endregion
+
     internal class PayModule
     {
         [RequestPacketHandler("GetPurchaseListRequest")]
         public static void GetPurchaseListRequestHandler(Session session, Packet.Request packet)
         {
-            GetPurchaseListResponse getPurchaseListResponse = new();
-            session.SendResponse(getPurchaseListResponse, packet.Id);
+            session.SendResponse(new GetPurchaseListResponse(), packet.Id);
+        }
+
+        [RequestPacketHandler("PayInitiatedRequest")]
+        public static void PayInitiatedRequest(Session session, Packet.Request packet)
+        {
+            session.SendResponse(new PayInitiatedResponse() { Code = 1 }, packet.Id);
         }
     }
 }
