443a4393a5b0c64ae1e65d6cbb3bcf41579145f0
Kyle Belanger <kyle873@gmail.com>
2024-04-13 00:24:12 -0400

    GetCourseRewardRequest stub to prevent softlock on course reward claim

--- AscNet.GameServer/Handlers/TaskModule.cs
+++ AscNet.GameServer/Handlers/TaskModule.cs
@@ -1,17 +1,37 @@
 ﻿using AscNet.Common.MsgPack;
+using MessagePack;
 
 namespace AscNet.GameServer.Handlers
 {
+
+    #region MsgPackScheme
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    [MessagePackObject(true)]
+    public class GetCourseRewardRequest
+    {
+        public int StageId;
+    }
+    
+    [MessagePackObject(true)]
+    public class GetCourseRewardResponse
+    {
+        public int Code;
+    }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    #endregion
+
     internal class TaskModule
     {
         [RequestPacketHandler("DoClientTaskEventRequest")]
         public static void DoClientTaskEventRequestHandler(Session session, Packet.Request packet)
         {
-            DoClientTaskEventResponse doClientTaskEventResponse = new()
-            {
-                Code = 0
-            };
-            session.SendResponse(doClientTaskEventResponse, packet.Id);
+            session.SendResponse(new DoClientTaskEventResponse(), packet.Id);
+        }
+        
+        [RequestPacketHandler("GetCourseRewardRequest")]
+        public static void GetCourseRewardRequestHandler(Session session, Packet.Request packet)
+        {
+            session.SendResponse(new GetCourseRewardResponse() { Code = 1 }, packet.Id);
         }
     }
 }
